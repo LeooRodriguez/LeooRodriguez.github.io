48b5913040062ad770a1b836057a5fd1f0ad895a
Leonardo Rodríguez <71232249+LeooRodriguez@users.noreply.github.com>
2024-05-09 21:15:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
+        habilidades[0].classList.add("java");
         habilidades[1].classList.add("htmlcss");
         habilidades[2].classList.add("python");
         habilidades[3].classList.add("javascript");
